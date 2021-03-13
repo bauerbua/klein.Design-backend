be01b88ae976420ab5ddb8c9e3ef22022ff3e927
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-03-13 22:37:01 +0100

    fixed aussteller and newsletter

--- api/aussteller/controllers/aussteller.js
+++ api/aussteller/controllers/aussteller.js
@@ -5,4 +5,36 @@
  * to customize this controller
  */
 
-module.exports = {};
+const {parseMultipartData, sanitizeEntity} = require("strapi-utils");
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const {data, files} = parseMultipartData(ctx);
+      entity = await strapi.services.aussteller.create(data, { files });
+    } else {
+      const {vorname, nachname, email, titel, beschreibung} = ctx.request.body;
+      entity = await strapi.services.aussteller.create({
+        vorname,
+        nachname,
+        email,
+        titel,
+        beschreibung,
+        published_at: null
+      });
+    }
+
+    entity = sanitizeEntity(entity, {model: strapi.models.aussteller});
+
+    await strapi.plugin['email'].services.email.send({
+      to: 'bauerjakob17@gmail.com',
+      from: 'bauer.j99@gmx.at',
+      subject: 'BEWERBUNG AUSSTELLER',
+      priority: 'high',
+      text: ctx.request.body
+    })
+
+    return entity
+  }
+}
