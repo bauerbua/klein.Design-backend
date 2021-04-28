c1858f641896a644087c46a6722111b26179b1f5
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-04-28 23:45:47 +0200

    #11 [feat] deleting related images when aussteller entry is deleted  - a bit buggy right now

--- api/aussteller/models/aussteller.js
+++ api/aussteller/models/aussteller.js
@@ -9,29 +9,40 @@ module.exports = {
 
   lifecycles: {
     async afterDelete(data) {
-      // delete titelbild
-      if (data.titelbild.related.length <= 1) {
-        const file = await strapi.plugins.upload.services.upload.fetch({id: data.titelbild.id});
-        await strapi.plugins.upload.services.upload.remove(file);
-      }
-
       // delete fotos
-      for (const foto of data.fotos) {
-        if (foto.related.length <= 1) {
-          const file = await strapi.plugins.upload.services.upload.fetch({id: foto.id});
+      if (data.fotos.length <= 1) {
+        for (const foto of data.fotos) {
+          if (foto.related.length <= 1) {
+            const file = await strapi.plugins.upload.services.upload.fetch({id: foto.id});
+            await strapi.plugins.upload.services.upload.remove(file);
+          }
+        }
+      }
+      // delete titelbild
+      if (data.titelbild) {
+        if (data.titelbild.related.length <= 1) {
+          const file = await strapi.plugins.upload.services.upload.fetch({id: data.titelbild.id});
           await strapi.plugins.upload.services.upload.remove(file);
         }
       }
     },
 
     async afterCreate(data) {
-      /*await strapi.plugins.email.services.email.send({
+      await strapi.plugins.email.services.email.send({
         to: 'bauerjakob17@gmail.com',
-        from: 'bauer.j99@gmx.at',
-        subject: 'BEWERBUNG AUSSTELLER',
-        priority: 'high',
-        text: 'email sent via strapi'
-      });*/
+        template_id: "d-bdc39ab2085945458dbe93accf05facb",
+        dynamic_template_data: {
+          vorname: data.vorname,
+          nachname: data.nachname,
+          firmenname: data.firmenname,
+          email: data.email,
+          telefonnummer: data.telefonnummer,
+          beschreibung: data.beschreibung,
+          standplatz: data.standplatz
+        }
+      }).catch(err => {
+        console.log(err);
+      })
     }
 
   }
