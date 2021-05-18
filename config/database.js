6ead91820420f9e59bf6b4d5708968a1851fad01
jakob.bauer <jakobbauer@thesiestagroup.com>
2021-05-18 23:49:35 +0200

    removed documentation plugin

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // PRODUCTION
-module.exports = ({ env }) => ({
+/*module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
@@ -19,9 +19,9 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
+*/
 //DEVELOPMENT
-/*module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
@@ -40,4 +40,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-});*/
+});
